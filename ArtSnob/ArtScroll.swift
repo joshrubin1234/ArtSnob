a0e4bad5e5bd4aab7f0442dd7555e214fcab6bcf
Josh Rubin <joshrubin2014@gmail.com>
2022-10-14 14:12:14 -0400

    button can append items to and remove items from the main list. (items refers to assets contained in a particular art category, example: pre-renaissance)

--- ArtSnob/ArtScroll.swift
+++ ArtSnob/ArtScroll.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ArtScroll: View {
+    var preRenaissanceList: [String] = ["pre", "pre"]
+    var earlyRenaissanceList: [String] = ["ear", "ear"]
+    e m
+    @State var list: [String] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            HStack{
+                if(!list.contains(preRenaissanceList)){
+                    Button(action: {
+                        list.append(contentsOf: preRenaissanceList);print(list);
+                    },label: {
+                        Text("Pre-renissance")
+                    })
+                }
+                else{
+                    
+                    Button(action: {
+                        list = list.filter{!preRenaissanceList.contains($0)};print(list)
+                    },label: {
+                        Text("Pre-renissance")
+                    })
+                }
+                if(!list.contains(earlyRenaissanceList)){
+                    Button(action: {
+                        list.append(contentsOf: earlyRenaissanceList);print(list);
+                    },label: {
+                        Text("early-renissance")
+                    })
+                }
+                else{
+                    Button(action: {
+                        list = list.filter{!earlyRenaissanceList.contains($0)};print(list)
+                    },label: {
+                        Text("early-renissance")
+                    })
+                }
+                
+
+            }
+        }
+        .padding()
+        
     }
 }
 
 struct ArtScroll_Previews: PreviewProvider {
     static var previews: some View {
         ArtScroll()
+        
     }
 }
