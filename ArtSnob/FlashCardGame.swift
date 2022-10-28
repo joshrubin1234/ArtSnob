7402f1e4235e56616a12d8d070a1bb54e95d7d63
Josh Rubin <joshrubin2014@gmail.com>
2022-10-28 12:10:35 -0400

    ArtScroll class has the ability to click through images

--- ArtSnob/FlashCardGame.swift
+++ ArtSnob/FlashCardGame.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct FlashCardGame: View {
+    @State private var bigCounter = 0
+    @State private var counter = 0
     var body: some View {
+    
         HStack{
-            let list = masterList[0]
-            let listo = list[0]
-            Image(listo.name).resizable().scaledToFit()
+//            Button(action: masterList.forEach{ list in
+//                list.forEach{ flashcard in
+//                    Image(flashcard.name).resizable().scaledToFit()
+//                }
+//            }, label: {
+//                Text("->")
+//            })
+//
+            var list = masterList[bigCounter]
+            var element = list[counter]
+            Image(element.name).resizable().scaledToFit()
+            
+            Button(action: {
+                if counter == list.count - 1{
+                    counter = 0
+                    bigCounter += bigCounter
+                }
+                counter = counter + 1
+
+            }, label: {
+                Text("->")
+            })
+        
             
         }
+      
+       
     }
 }
 
